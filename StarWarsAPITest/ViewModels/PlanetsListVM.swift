ce71c62c96e75ac44aaac5815d55288c8ef345e1
Kent Franks <kent.franks@lpl.com>
2019-03-07 12:44:07 -0500

    Remove commented code

--- StarWarsAPITest/ViewModels/PlanetsListVM.swift
+++ StarWarsAPITest/ViewModels/PlanetsListVM.swift
@@ -29,39 +29,7 @@ class PlanetsListVM {
         requests.append(request3)
     }
     
-    // MARK: - Fetch functions
-//    func fetchStarWarsPlanets(completion: @escaping () -> Void) {
-//        serverConnection?.execute(withMultipleAsyncRequestsOfTheSameType: requests, and: .get) {
-//            (response, error) in
-//            if let _ = error {
-//                // present alert
-//                return
-//            }
-//            guard let fetchPlanetsResponse1 = response?[0] as? FetchPlanetsResponse else {
-//                // present alert
-//                return
-//            }
-//            guard let fetchPlanetsResponse2 = response?[1] as? FetchPlanetsResponse else {
-//                // present alert
-//                return
-//            }
-//            guard let fetchPlanetsResponse3 = response?[2] as? FetchPlanetsResponse else {
-//                // present alert
-//                return
-//            }
-//            self.starWarsPlanetsArray = fetchPlanetsResponse1.planets
-//            self.starWarsPlanetsArray += fetchPlanetsResponse2.planets
-//            self.starWarsPlanetsArray += fetchPlanetsResponse3.planets
-//            self.starWarsPlanetsArray = self.starWarsPlanetsArray.sorted {
-//                guard let name1 = $0.name, let name2 = $1.name else {
-//                    return false
-//                }
-//                return name1 < name2
-//            }
-//            completion()
-//        }
-//    }
- 
+    // MARK: - Fetch
     func fetchStarWarsPlanets(completion: @escaping () -> Void) {
         serverConnection?.execute(withMultipleAsyncRequestsOfDifferentTypes: requests, and: .get) {
             (response, error) in
